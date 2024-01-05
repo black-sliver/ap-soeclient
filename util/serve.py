1115960cb7e4e08febd48aa6d904c37b1216aeda
black-sliver <59490463+black-sliver@users.noreply.github.com>
2024-01-05 18:48:48 +0100

    add option for ssl to util/serve.py

--- util/serve.py
+++ util/serve.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
 import http.server
+import os.path
 import socketserver
+from pathlib import Path
 
-PORT = 8000
+USE_SSL = False  # create key.pem and cert.pem and set this to True to host via https://
+PORT = 4443 if USE_SSL else 8000
+
+script_dir = Path(os.path.dirname(os.path.realpath(__file__)))
 
 Handler = http.server.SimpleHTTPRequestHandler
 Handler.extensions_map.update({
@@ -13,5 +18,11 @@ Handler.extensions_map.update({
 socketserver.TCPServer.allow_reuse_address = True
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     httpd.allow_reuse_address = True
+    if USE_SSL:
+        import ssl
+        httpd.socket = ssl.wrap_socket(httpd.socket,
+                                       keyfile=script_dir / "key.pem",
+                                       certfile=script_dir / "cert.pem",
+                                       server_side=True)
     print("serving at port", PORT)
     httpd.serve_forever()
